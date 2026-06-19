29f7dd765fc723713c2a38180caff640d2574de8
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-19 21:24:06 +0200

    install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
 export default function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./assets/css/index.css";
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
