c4b1d9ca86337e101439bed626410ae4ea86d6db
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-22 20:34:37 +0200

    create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <>
+      <section id="footer" className="bg-dark text-light p-5">
+        <div className="container">
+          <div className="row row-cols-3 text-center">
+            <div className="col">Placeholder</div>
+            <div className="col">Placeholder</div>
+            <div className="col">Placeholder</div>
+          </div>
+          <div className="row row-cols-3 text-center">
+            <div className="col">Placeholder</div>
+            <div className="col">Placeholder</div>
+            <div className="col">Placeholder</div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import { Carousel } from "bootstrap";
 import { useEffect } from "react";
+import Footer from "../components/Footer";
 
 export default function DefaultLayout() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
