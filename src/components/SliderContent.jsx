fc23ab125d2b35c176ea67233a5817cf2b56d8e7
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-22 19:12:13 +0200

    fixing slider

--- src/components/SliderContent.jsx
+++ src/components/SliderContent.jsx
@@ -1,24 +1,30 @@
 export default function SliderContent({ activeIndex, sliderImage }) {
   return (
-    <section>
-      {sliderImage.map((slide, index) => {
-        return (
-          <div
-            key={index}
-            className={`${index === activeIndex ? "slides active" : "inactive"}`}
-          >
+    <section className="slider-wrapper">
+      <div
+        className="slider-content"
+        style={{
+          transform: `translateX(-${activeIndex * 100}%)`,
+        }}
+      >
+        {sliderImage.map((slide, index) => (
+          <div className="slides" key={index}>
             <div className="layover"></div>
+
             <img src={slide.imgUrl} alt="" className="slide-image" />
+
             <div className="info-container d-flex flex-column justify-content-between text-center gap-4 align-items-center">
               <h1 className="slide-title">{slide.title}</h1>
+
               <p className="slide-text">{slide.paragraph}</p>
+
               <button className="btn btn-outline-light rounded-pill align-self-stretch mx-5 fw-bold">
                 {slide.btnText}
               </button>
             </div>
           </div>
-        );
-      })}
+        ))}
+      </div>
     </section>
   );
 }
