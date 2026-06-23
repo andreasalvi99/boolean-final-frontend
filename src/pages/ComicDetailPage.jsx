5dd987cb410927655c166e001b7da0c1efadd094
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-23 19:22:49 +0200

    add link to character in comic detail

--- src/pages/ComicDetailPage.jsx
+++ src/pages/ComicDetailPage.jsx
@@ -148,20 +148,25 @@ export default function ComicDetailPage() {
                 <div className="row row-cols-6 g-3">
                   {comic.characters.map((character) => {
                     return (
-                      <div className="col" key={character.id}>
-                        <div className="card border-0 related-character-card h-100">
-                          <img
-                            src={`http://127.0.0.1:8000/storage/${character.character_img}`}
-                            className="card-img-top h-100"
-                            alt="..."
-                          />
-                          <div className="card-body related-character-info">
-                            <p className="card-text text-center">
-                              {character.name}
-                            </p>
+                      <Link
+                        key={character.id}
+                        to={`/characters/${character.id}`}
+                      >
+                        <div className="col">
+                          <div className="card border-0 related-character-card h-100">
+                            <img
+                              src={`http://127.0.0.1:8000/storage/${character.character_img}`}
+                              className="card-img-top h-100"
+                              alt="..."
+                            />
+                            <div className="card-body related-character-info">
+                              <p className="card-text text-center">
+                                {character.name}
+                              </p>
+                            </div>
                           </div>
                         </div>
-                      </div>
+                      </Link>
                     );
                   })}
                 </div>
