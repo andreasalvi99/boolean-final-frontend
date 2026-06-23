2dd60e1d25cf8c5c898647d93142f91cc85b5406
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-23 16:07:29 +0200

    map characters card w/ animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,62 +17,60 @@ export default function Footer() {
             </div>
             <div className="col-3">
               <div className="d-flex justify-content-center align-items-center flex-column gap-3 h-100">
-                <a href="" className="text-decoration-none text-light">
+                <a href="#" className="text-decoration-none text-light">
                   Placeholder
                 </a>
-                <a href="" className="text-decoration-none text-light">
+                <a href="#" className="text-decoration-none text-light">
                   Placeholder
                 </a>
-                <a href="" className="text-decoration-none text-light">
+                <a href="#" className="text-decoration-none text-light">
                   Placeholder
                 </a>
-                <a href="" className="text-decoration-none text-light">
+                <a href="#" className="text-decoration-none text-light">
                   Placeholder
                 </a>
-                <a href="" className="text-decoration-none text-light">
-                  Placeholder
-                </a>
-              </div>
-            </div>
-            <div className="col-3">
-              <div className="d-flex justify-content-center align-items-center flex-column gap-3 h-100">
-                <a href="" className="text-decoration-none text-light">
-                  Placeholder
-                </a>
-                <a href="" className="text-decoration-none text-light">
-                  Placeholder
-                </a>
-                <a href="" className="text-decoration-none text-light">
-                  Placeholder
-                </a>
-                <a href="" className="text-decoration-none text-light">
-                  Placeholder
-                </a>
-                <a href="" className="text-decoration-none text-light">
+                <a href="#" className="text-decoration-none text-light">
                   Placeholder
                 </a>
               </div>
             </div>
             <div className="col-3">
               <div className="d-flex justify-content-center align-items-center flex-column gap-3 h-100">
-                <h4 href="" className="text-decoration-none text-light">
-                  Social
-                </h4>
+                <a href="#" className="text-decoration-none text-light">
+                  Placeholder
+                </a>
+                <a href="#" className="text-decoration-none text-light">
+                  Placeholder
+                </a>
+                <a href="#" className="text-decoration-none text-light">
+                  Placeholder
+                </a>
+                <a href="#" className="text-decoration-none text-light">
+                  Placeholder
+                </a>
+                <a href="#" className="text-decoration-none text-light">
+                  Placeholder
+                </a>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="d-flex justify-content-center align-items-center flex-column gap-3 h-100">
+                <h4 className="text-decoration-none text-light">Social</h4>
                 <div className="d-flex justidy-content-between align-items-center gap-3 fs-2">
                   <span>
-                    <i class="bi bi-instagram"></i>
+                    <i className="bi bi-instagram"></i>
                   </span>
                   <span>
-                    <i class="bi bi-facebook"></i>
+                    <i className="bi bi-facebook"></i>
                   </span>
                   <span>
-                    <i class="bi bi-twitter-x"></i>
+                    <i className="bi bi-twitter-x"></i>
                   </span>
                   <span>
-                    <i class="bi bi-reddit"></i>
+                    <i className="bi bi-reddit"></i>
                   </span>
                   <span>
-                    <i class="bi bi-discord"></i>
+                    <i className="bi bi-discord"></i>
                   </span>
                 </div>
               </div>
--- src/pages/CharactersPage.jsx
+++ src/pages/CharactersPage.jsx
@@ -1,3 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function CharactersPage() {
-  return <></>;
+  const [characters, setCharacters] = useState([]);
+
+  function fetchCharacters() {
+    axios.get("http://127.0.0.1:8000/api/characters").then((response) => {
+      //   console.log(response.data.results);
+      setCharacters(response.data.results);
+    });
+  }
+
+  useEffect(fetchCharacters, []);
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="gallery">
+            {characters.map((character) => {
+              return (
+                <div
+                  className="panel"
+                  style={{
+                    backgroundImage: `url(http://127.0.0.1:8000/storage/${character.character_img})`,
+                  }}
+                  key={character.id}
+                ></div>
+
+                // <div className="col" key={character.id}>
+                //   <div className="card h-100">
+                //     <img
+                //       src={`http://127.0.0.1:8000/storage/${character.character_img}`}
+                //       className="card-img-top h-100"
+                //       alt="..."
+                //     />
+                //   </div>
+                // </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
