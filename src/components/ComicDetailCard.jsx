9473c2b10e48f6de981ae88a5d9ba4d4604f6b55
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-25 19:17:49 +0200

    add card in comic detail

--- src/components/ComicDetailCard.jsx
+++ src/components/ComicDetailCard.jsx
@@ -1,3 +1,5 @@
+import dateFormat from "dateformat";
+
 export default function ComicDetailCard({
   cover,
   title,
@@ -7,5 +9,59 @@ export default function ComicDetailCard({
   brand,
   price,
 }) {
-  return <></>;
+  return (
+    <div className="card mb-3 comic-card">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img
+            src={`http://127.0.0.1:8000/storage/${cover}`}
+            className="img-fluid rounded-start w-100 h-100"
+            alt={title}
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body ps-5 d-flex flex-column justify-content-between h-100">
+            <h3 className="card-title">{title}</h3>
+            <p
+              className="card-text overflow-auto"
+              style={{ minHeight: "100px" }}
+            >
+              {description}
+            </p>
+            <p className="card-text">
+              Pubblicazione:
+              <span className="text-light">
+                {dateFormat(release_date, " dddd, mmmm dS, yyyy")}
+              </span>
+            </p>
+            <div>
+              Featuring:
+              <span>
+                {characters?.map((character, index) => {
+                  return (
+                    " " +
+                    character.name +
+                    (index < characters.length - 1 ? ", " : "")
+                  );
+                })}
+              </span>
+            </div>
+            <div className="d-flex justify-content-between align-items-end">
+              <img
+                src={`http://127.0.0.1:8000/storage/${brand?.logo}`}
+                alt=""
+                style={{
+                  height: brand?.name === "DC Comics" ? "80px" : "100px",
+                  width: brand?.name === "Marvel Comics" ? "200px" : "100px",
+                }}
+              />
+              <p className="align-self-end m-0 fs-1 fw-semibold">
+                <i>&euro; {price}</i>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/ComicDetailPage.jsx
+++ src/pages/ComicDetailPage.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import dateFormat from "dateformat";
 import Loader from "../components/Loader";
 import GoBackBtn from "../components/GoBackBtn";
+import ComicDetailCard from "../components/ComicDetailCard";
 
 export default function ComicDetailPage() {
   const { id } = useParams();
@@ -47,70 +47,15 @@ export default function ComicDetailPage() {
                 id="comic-detail"
               >
                 <div className="container">
-                  <div className="card mb-3 comic-card">
-                    <div className="row g-0">
-                      <div className="col-md-4">
-                        <img
-                          src={`http://127.0.0.1:8000/storage/${comic.cover_img}`}
-                          className="img-fluid rounded-start w-100 h-100"
-                          alt={comic.title}
-                        />
-                      </div>
-                      <div className="col-md-8">
-                        <div className="card-body ps-5 d-flex flex-column justify-content-between h-100">
-                          <h3 className="card-title">{comic.title}</h3>
-                          <p
-                            className="card-text overflow-auto"
-                            style={{ minHeight: "100px" }}
-                          >
-                            {comic.description}
-                          </p>
-                          <p className="card-text">
-                            Pubblicazione:
-                            <span className="text-light">
-                              {dateFormat(
-                                comic.release_date,
-                                " dddd, mmmm dS, yyyy",
-                              )}
-                            </span>
-                          </p>
-                          <div>
-                            Featuring:
-                            <span>
-                              {comic.characters?.map((character, index) => {
-                                return (
-                                  " " +
-                                  character.name +
-                                  (index < comic.characters.length - 1
-                                    ? ", "
-                                    : "")
-                                );
-                              })}
-                            </span>
-                          </div>
-                          <div className="d-flex justify-content-between align-items-end">
-                            <img
-                              src={`http://127.0.0.1:8000/storage/${comic.brand?.logo}`}
-                              alt=""
-                              style={{
-                                height:
-                                  comic.brand?.name === "DC Comics"
-                                    ? "80px"
-                                    : "100px",
-                                width:
-                                  comic.brand?.name === "Marvel Comics"
-                                    ? "200px"
-                                    : "100px",
-                              }}
-                            />
-                            <p className="align-self-end m-0 fs-1 fw-semibold">
-                              <i>&euro; {comic.price}</i>
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <ComicDetailCard
+                    cover={comic.cover_img}
+                    title={comic.title}
+                    description={comic.description}
+                    release_date={comic.release_date}
+                    characters={comic.characters}
+                    brand={comic.brand}
+                    price={comic.price}
+                  />
                 </div>
               </section>
 
