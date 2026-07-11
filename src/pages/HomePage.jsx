bf1ae33a2035e8091f0e92a365a846b5b8ac5249
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-07-11 16:49:36 +0200

    set latest comics section in homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,56 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Slider from "../components/slider/Slider";
+import axios from "axios";
+import ComicCard from "../components/ComicCard";
+
 export default function HomePage() {
+  const [latestComics, setLatestComics] = useState([]);
+
+  function fetchLatestComics() {
+    axios.get("http://127.0.0.1:8000/api/comics").then((response) => {
+      // console.log(response);
+
+      const results = response.data.data;
+
+      const filteredLatest = results.filter((result) => result.is_new === 1);
+      // console.log("filteredLatest", filteredLatest);
+
+      setLatestComics(filteredLatest);
+    });
+  }
+
+  const comicIds = latestComics.map((comic) => comic.id);
+
+  console.log(latestComics);
+
+  useEffect(fetchLatestComics, []);
+
   return (
     <>
       <section id="main-content">
         <Slider />
+
+        {latestComics && (
+          <section id="latest-comics" className="p-4">
+            <div className="container">
+              <h1 className="bangers-regular">Ultimi arrivi:</h1>
+              <div className="row row-cols-5">
+                {latestComics.map((comic) => {
+                  return (
+                    <ComicCard
+                      id={comic.id}
+                      key={comic.id}
+                      cover={comic.cover_img}
+                      title={comic.title}
+                      comicIds={comicIds}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </section>
+        )}
       </section>
     </>
   );
