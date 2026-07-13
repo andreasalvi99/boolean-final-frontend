cd2388f31138b972de47c80ed8b4b0f58bf58c9e
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-07-13 20:52:54 +0200

    change discount badge fs

--- src/components/ComicDetailCard.jsx
+++ src/components/ComicDetailCard.jsx
@@ -91,7 +91,7 @@ export default function ComicDetailCard({
                       <span className="text-decoration-line-through oswald-special">
                         &euro; {price}
                       </span>
-                      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger fs-5">
+                      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger fs-6">
                         -{isDiscount}%
                       </span>
                       <span className="d-block oswald-special text-danger">
