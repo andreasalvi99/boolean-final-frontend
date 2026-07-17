6f71d188f8c27c53bb28325e5453dc2f65a78a87
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-07-17 12:29:01 +0200

    add scrolltotop for windows display debug

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,12 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import { Carousel } from "bootstrap";
 import { useEffect } from "react";
 import Footer from "../components/Footer";
 
 export default function DefaultLayout() {
+  function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  }
+
   return (
     <>
+      <ScrollToTop />
+
       <Header />
       <Outlet />
       <Footer />
