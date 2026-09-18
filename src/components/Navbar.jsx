cc09a39ddf8b91bf96a60b6dbf24faa4bc393a48
Andrea Salvi <andrea.salvi.r@gmail.com>
2026-09-18 16:50:05 +0200

    add offcanvas for cart view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,53 +17,67 @@ export default function Navbar() {
     }, []);
   }
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top bebas-neue-regular">
-      <div className="container">
-        <NavLink to="/">
-          <img src={logo} alt="" className="navbar-logo" />
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          onClick={report}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        {/* <button
-          className="navbar-toggler"
-          type="button"
-          onClick={() => {
-            document
-              .querySelector("#navbarSupportedContent")
-              .classList.toggle("show");
-          }}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button> */}
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink to="/comics" className="nav-link">
-                Fumetti
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/characters" className="nav-link">
-                Personaggi
-              </NavLink>
-            </li>
-          </ul>
-          <div className="d-flex gap-4 align-items-center">
-            <SearchForm></SearchForm>
-            <i class="bi bi-handbag"></i>
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top bebas-neue-regular">
+        <div className="container">
+          <NavLink to="/">
+            <img src={logo} alt="" className="navbar-logo" />
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={report}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          {/* <button
+            className="navbar-toggler"
+            type="button"
+            onClick={() => {
+              document
+                .querySelector("#navbarSupportedContent")
+                .classList.toggle("show");
+            }}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button> */}
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to="/comics" className="nav-link">
+                  Fumetti
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/characters" className="nav-link">
+                  Personaggi
+                </NavLink>
+              </li>
+            </ul>
+            <div className="d-flex gap-4 align-items-center">
+              <SearchForm></SearchForm>
+              <button className="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                <i class="bi bi-handbag"></i>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasRightLabel">Il tuo carrello</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      ...
+    </div>
+  </div>
+</>
   );
 }
