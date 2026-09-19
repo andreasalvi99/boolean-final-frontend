e672e2916f05f562cf4fcfe26ae708263f36f220
Andrea Salvi <andrea.salvi.r@gmail.com>
2026-09-19 15:08:30 +0200

    add local storage to save cart

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -15,7 +15,15 @@ function ScrollToTop() {
 }
 
 export default function DefaultLayout() {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+  const savedCart = localStorage.getItem("cart");
+
+  return savedCart ? JSON.parse(savedCart) : [];
+});
+
+useEffect(() => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}, [cart]);
 
   return (
     <>
