f01abe19b5cd4a6b42744f6b43d5493779119b8f
Andrea Salvi <andrea.salvi.r@gmail.com>
2026-09-22 16:04:56 +0200

    set order success page

--- src/pages/OrderSuccess.jsx
+++ src/pages/OrderSuccess.jsx
@@ -1,98 +1,80 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function OrderSuccess() {
 
     const { orderId } = useParams()
-
-    // console.log(cart);
+    const [order, setOrder] = useState(null)
     
     function fetchOrderDetails() {
         axios.get(`https://laravel-final-backend.onrender.com/api/orders/${orderId}`).then((response) => {
-            console.log(response);
+            setOrder(response.data.order)
         }).catch((error) => {
             console.log(error.response.data);
         })
     }
+
+    const cartAmount = order?.comics?.reduce((total, comic) => {
+        return(
+             total + (comic.pivot.quantity * Number(comic.pivot?.price))
+        )
+    }, 0) ?? 0
+
+    const shippingPrice = order ? Number(order.total) - cartAmount : 0
 
     useEffect(fetchOrderDetails, [orderId])
 
     return(
         <>
             <h1 className="mt-3 bangers-regular">Complimenti!!</h1>
-            <p className="oswald-special fs-5">Il tuo ordine è stato inviato, a breve riceverai una mail di conferma con il link di tracciamento. Grazie per aver scelto Nextpanel.</p>
+            <p className="oswald-special fs-5">Il tuo ordine è stato inviato, a breve riceverai una mail di conferma con il link per il tracciamento. Grazie per aver scelto Nextpanel.</p>
         
             <h2>Riepilogo ordine:</h2>
-            <div className="row mt-5 fs-5 oswald-special">
-                    <div className="col-8 bg-light rounded p-2 border border-dark border-opacity-25">
-                        <table className="table table-sm mb-0 table-light">
-                            <thead>
-                                <tr>
-                                <th scope="col">Prodotto</th>
-                                <th scope="col">Subtotale</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    {/* <td>
-                                        {cart.map((item) => {
-                                            return(
-                                                <ul className="list-group" key={item.comic.id}>
-                                                    <li className="list-group">
-                                                        <div className="d-flex justify-content-between align-items-center gap-5 me-5">
-                                                            <span>{item.comic.title}</span>
-                                                            <span>x {item.quantity}</span>
-                                                        </div>
-                                                    </li>
-                                                </ul>
-                                        )})}
-                                    </td>
-                                    <td>
-                                        {cart.map((item) => {
-                                            return(
-                                            <ul className="list-group" key={item.comic.id}>
-                                                <li className="list-group">
-                                                    <div className="d-flex justify-content-between align-items-center gap-5 me-5">
-                                                        <span>&euro; {(item.comic.price * item.quantity).toFixed(2)}</span>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                    )})}
-                                    </td> */}
-                                </tr>
-                                {/* <tr>
-                                    <td>
-                                        <div className="d-flex align-items-center">
-                                            <span>Spedizione</span>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="shipping_method" id="shipping_standard" value="standard" onChange={handleChanges} checked={formData.shipping_method === "standard"}/>
-                                            <label className="form-check-label" htmlFor="shipping_standard">
-                                                Standard
-                                            </label>
-                                        </div>
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="shipping_method" id="shipping_express" value="express" onChange={handleChanges} checked={formData.shipping_method === "express"}/>
-                                            <label className="form-check-label" htmlFor="shipping_express">
-                                                Express (5 gg): {expressShippingFee}&euro;
-                                            </label>
-                                        </div>
-                                    </td>
-                                </tr> */}
-                                <tr>
+            <div className="row my-4">
+                <div className="col-8 bg-light rounded p-2 border border-dark border-opacity-25">
+                    <table className="table table-light table-borderless align-middle">
+                        <thead className="border-bottom">
+                            <tr>
+                            <th scope="col">Prodotto</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
+                            <th scope="col">Subtotale</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {order?.comics?.length > 0 && (
+                                order?.comics?.map((comic) => {
+                                    return(
+                                        <tr key={comic.id}>
+                                            <td scope="row">
+                                                <img src={`https://laravel-final-backend.onrender.com/img/${comic.cover_img}`} alt="" style={{height: "60px", width: "45px"}}/>
+                                            </td>
+                                            <td>{comic.title}</td>
+                                            <td>x{comic.pivot.quantity}</td>
+                                            <td>{comic.pivot.quantity * comic.price}&euro;</td>
+                                        </tr>
+                                    )
+                                })
+                            )}
+                            <tr className="border-top border-bottom">
+                                <td>Spedizione</td>
+                                <td>{order?.shipping_method}</td>
+                                <td></td>
+                                <td>{shippingPrice.toFixed(2)}&euro;</td>
+                            </tr>
+                        </tbody>
+                        <tfoot className="border-top">
+                            <tr>
                                 <td>Totale</td>
-                                <td>
-                                    {/* &euro; {total}  */}
-                                </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                                <td></td>
+                                <td></td>
+                                <td>{order?.total}&euro;</td>
+                            </tr>
+                        </tfoot>
+                    </table>
                 </div>
-        
+            </div>
         </>
     )
 }
