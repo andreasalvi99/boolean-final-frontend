28550b7507eaa82401abfbcc425df23d4ad07dbd
Andrea Salvi <andrea.salvi.r@gmail.com>
2026-09-22 22:18:12 +0200

    remove useless code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,17 +24,6 @@ export default function Navbar({ cart, setCart }) {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          {/* <button
-            className="navbar-toggler"
-            type="button"
-            onClick={() => {
-              document
-                .querySelector("#navbarSupportedContent")
-                .classList.toggle("show");
-            }}
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button> */}
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
